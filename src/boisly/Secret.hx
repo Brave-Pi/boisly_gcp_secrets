cab9faecc43dad260a31f4403fcf08429111db00
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2022-01-16 12:14:44 -0600

    Idk enough about Haxe macro context switches

--- src/boisly/Secret.hx
+++ src/boisly/Secret.hx
@@ -1,9 +1,12 @@
 package boisly;
+#if js
 import tink.json.Representation;
 import tink.CoreApi;
 import google_cloud.secret_manager.build.protos.protos.google.cloud.secretmanager.v1.IAccessSecretVersionResponse;
+#end
 @:jsonParse(function(str:String):boisly.Secret return str)
-abstract Secret(String){
+  abstract Secret(String){
+  #if js
     inline function new(v) this = v;
     @:from public static function ofString(v:String) return new Secret(v);
 	@:to public function reveal():Promise<tink.Chunk> {
@@ -12,4 +15,5 @@ abstract Secret(String){
 		}).then(d -> (d.element0.payload.data : Dynamic))
         .then((buf:js.node.Buffer) -> (buf : tink.Chunk));
 	}
+  #end
 }
